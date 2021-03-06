7898f4aa007ed7b0e684129e5b4ecde47d95f3a8
Jsto19 <stoney19llc@outlook.com>
2021-03-05 21:02:20 -0600

    First commit
    "first"

--- app/routes.py
+++ app/routes.py
@@ -13,6 +13,40 @@ def index():
     return render_template('index.html', title=title)
 
 # @app.route('/')
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    title = "Dale's Dead Bugs Service | register"
+    form = UserInfoForm()
+    if request.method == 'POST' and form.validate():
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data 
+        phone = form.phone.data
+        address = form.address.data
+        city = form.city.data
+        zipcode = form.zipcode.data
+        admin = form.admin.data
+        # print(username, email, password)
+
+        # create new instance of User
+        new_user = User(username, email, password, phone, address, city, zipcode, admin)
+        # add new instance to our database
+        db.session.add(new_user)
+        # commit database
+        db.session.commit()
+
+        # send email to new user
+        msg = Message(f"Welcome, {username}", [email])
+        msg.body = 'Thank you for signing up for the most glorious death of your bugs. I hope you enjoy your new carnage!'
+        msg.html = "<p>Thanks you so much for signing up for the Dale's Dead Bugs service. Where we do buggin right! We may, or may not, be in Liberia!</p>"
+
+        mail.send(msg)
+
+        flash("It may be a crack in your internet, or the Chinese are making their move!", "success")
+        return redirect(url_for('hello_world'))
+    return render_template('register.html', title=title, form=form)
+
+
 
 # @app.route('/shoppingCart', methods=["GET", "POST"])
 # @login_required
