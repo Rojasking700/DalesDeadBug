d2a0dee84ed02097d323d522a54deb4025bdd0d7
gabriel rojas <grojaspiano@gmail.com>
2021-03-06 14:52:16 -0600

    working updates

--- app/routes.py
+++ app/routes.py
@@ -11,7 +11,6 @@ ddb = "Dale's Dead Bugs | "
 @app.route('/index')
 def index():
     title = ddb + 'HOME'
-
     return render_template('index.html', title=title)
 
 # @app.route('/')
@@ -47,29 +46,6 @@ def register():
         flash("It may be a crack in your internet, or the Chinese are making their move!", "success")
         return redirect(url_for('index'))
     return render_template('register.html', title=title, form=form)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    title = "Kekambas Blog | LOGIN"
-    form = LoginForm()
-    if request.method == 'POST' and form.validate():
-        username = form.username.data
-        password = form.password.data
-
-        user = User.query.filter_by(username=username).first()
-
-        if user is None or not check_password_hash(user.password, password):
-            flash("Incorrect Email/Password. Please try again", 'danger')
-            return redirect(url_for('login'))
-        
-        login_user(user, remember=form.remember_me.data)
-        flash("You have successfully logged in!", 'success')
-        next_page = request.args.get('next')
-        if next_page:
-            return redirect(url_for(next_page.lstrip('/')))
-        return redirect(url_for('index'))
-
-    return render_template('login.html', title=title, form=form)
 
 @app.route('/logout')
 def logout():
